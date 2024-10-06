6d2855b63f212c8388f6b76c1e716058f362cbe0
Nikolai <gorbachev.nm@gmail.com>
2024-10-06 13:42:01 +0400

    substituted google maps with leaflet maps

--- xmlParser.js
+++ xmlParser.js
@@ -18,6 +18,7 @@ function parseXML(kind='render', xmlPath="XML/Madorski_2.0.xml", elementToStickT
     const parser = new DOMParser();
     const doc = parser.parseFromString(text, "text/xml");
     personXMLTree = doc.documentElement.querySelector('profileDesc particDesc listPerson person')
+    geoXMLTree = doc.documentElement.querySelector('profileDesc settingDesc listPlace')
     rdfDescriptionTree = doc.documentElement.querySelector('xenoData RDF Description')
     if (kind === 'render' && xmlRenderedOnPage === false){
         xmlRenderedOnPage = true
@@ -122,7 +123,91 @@ function parseXML(kind='render', xmlPath="XML/Madorski_2.0.xml", elementToStickT
         }
         downloadCSVFile(csv_text);
 
+    } else if (kind === 'map'){
+
+        node = document.getElementById(elementToStickToID);
+
+        innerHTMLToAdd = ''
+        
+
+        console.log('here')
+
+        
+
+
+        level = 0
+        geoList = []
+        for (n of geoXMLTree.childNodes) {
+            
+        
+            if (n.nodeName === 'place') {
+                level += 1 
+                
+                const name = n.querySelector('placeName')?.innerHTML
+                const geo = n.querySelector('location geo')?.innerHTML
+                
+                let geoDesc = {
+                    'name': '',
+                    'geo': [0,0]
+                }
+
+                if (!!name) {
+                    geoDesc['name'] = name
+                    // innerHTMLToAdd += '<div style="display: flex; flex-direction: row; justify-content: space-between; width: 100%; margin-top: 0.5rem">'+'<div>'+'Education Level ' + level +':</div>'+'<div>'+name+'</div>'+'</div>'
+                }
+                if (!!geo) {
+                    geoDesc['geo'][0] = geo.split(',')[0]
+                    geoDesc['geo'][1] = geo.split(',')[1]
+                    // innerHTMLToAdd += '<div style="display: flex; flex-direction: row; justify-content: space-between; width: 100%;">'+'<div>'+'Graduation Date' +':</div>'+'<div>'+geo+'</div>'+'</div>'
+                }
+
+                geoList.push(geoDesc)
+                   
+
+                console.log(name, geo)
+
+                
+            }
+        }
+        console.log(geoList)
+
+
+        let x0 = geoList[0]['geo'][0]
+        let y0 = geoList[0]['geo'][1]
+        var map = L.map('map').setView([x0, y0], 2);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+
+
+        for (place of geoList) {
+            L.marker([place['geo'][0], place.geo[1]]).addTo(map)
+                .bindPopup(place['name'])
+                .openPopup();
+        }
+        
+        mapNode = document.getElementById('map');
+
+        innerHTMLToAdd = ''
+
+        for (n of personXMLTree.childNodes) {
+            if (n.nodeName === 'persName') {
+                innerHTMLToAdd += 'Key places for '+n.innerHTML+' Career'
+            }
+
+            
+
+        }
+
+
+        mapNode.insertAdjacentHTML('beforebegin', '<div style="display: flex; text-align: left; align-items: center; justify-content: center; width: 100%;  flex-direction: row; ">'+'<div style="display: flex; justify-content: center;  flex-direction: column; gap:0.2rem; margin-top: 0.8rem; width: 100%; font-family: Arial, sans-serif; font-size: 1.2rem; font-weight: 600">'+innerHTMLToAdd+'</div></div>');
+
+
     }
+
+
 
     });
 }
